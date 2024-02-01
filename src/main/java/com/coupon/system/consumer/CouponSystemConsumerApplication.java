7f5fad1724630bdd5d4866be977cfdbc83f3a60d
khyungjun <kpos7503@naver.com>
2024-02-01 16:54:37 +0900

    init project

---
+++ src/main/java/com/coupon/system/consumer/CouponSystemConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.coupon.system.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSystemConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponSystemConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coupon/system/consumer/CouponSystemConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupon.system.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSystemConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
